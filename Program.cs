0ff09a37cb4f1f6d6bd844245b10ba59c3c758b1
sEraysin <gulereray211@gmail.com>
2026-07-03 00:31:23 +0300

    Clean up template comments and unused assets

--- Program.cs
+++ Program.cs
@@ -5,40 +5,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// MVC ve Razor Pages servisleri
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-// Connection string'i appsettings.Development.json içinden alıyoruz
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// MySQL bağlantısı
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
-// Identity ayarları
 builder.Services.AddIdentity<AppUser, AppRole>(options =>
 {
-    // Şifre kuralları
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = true;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequiredLength = 6;
 
-    // Kullanıcı ayarları
     options.User.RequireUniqueEmail = true;
 
-    // Giriş deneme kilitleme ayarları
     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
     options.Lockout.MaxFailedAccessAttempts = 5;
 })
 .AddEntityFrameworkStores<ApplicationDbContext>()
 .AddDefaultTokenProviders();
 
-// Login yönlendirme ayarları
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
@@ -46,7 +38,6 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = "/Account/AccessDenied";
 });
 
-// Aktif/pasif değişikliği açık oturumlara hemen yansısın.
 builder.Services.Configure<SecurityStampValidatorOptions>(options =>
 {
     options.ValidationInterval = TimeSpan.Zero;
@@ -54,7 +45,6 @@ builder.Services.Configure<SecurityStampValidatorOptions>(options =>
 
 var app = builder.Build();
 
-// Hata yönetimi
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -66,11 +56,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// Önce authentication, sonra authorization
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Varsayılan route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
